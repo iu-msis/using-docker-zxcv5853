a7d0011e1cd16a98844af0301871c7f85724ee1a
zxcv5853 <37623021+zxcv5853@users.noreply.github.com>
2019-09-22 11:59:08 -0400

    Reload page with Submit button
    Use the VueJS method of capturing the `submit` event

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -11,8 +11,17 @@ methods: {
       .then(json => { waitingApp.patients = json })
     }
 },
+
+handleSubmit(event) {
+//  fetch(url, {
+//    method: 'post',
+//    data: this.recordPatient
+//  })
+//  .then(...)
+
+},
   created() {
-    this.fetchPatients();
+    this.fetchPatients()
 }
 
 });
